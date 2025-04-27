e77d88f1c890028d077d60c29642e4d35fb0c502
Carbone <rtcarbone@gmail.com>
2025-04-27 14:43:06 -0300

    chore: initialize project with Spring Boot and Maven setup

---
+++ src/main/java/br/com/fiap/javacare/JavacareApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.javacare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacareApplication.class, args);
+	}
+
+}
