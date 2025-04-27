b231416766480442d274c19a7cddd85a9e4e67c9
Carbone <rtcarbone@gmail.com>
2025-04-27 18:38:41 -0300

    feat: configure application properties for PostgreSQL and RabbitMQ

---
+++ history/src/main/java/br/com/fiap/javacare/history/HistoryApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.javacare.history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HistoryApplication.class, args);
+    }
+
+}
---
+++ scheduling/src/test/java/br/com/fiap/javacare/scheduling/SchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.javacare.scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
