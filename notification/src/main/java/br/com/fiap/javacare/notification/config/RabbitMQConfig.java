8022401fcf5c14f351162ef624f208c598f57ccf
Carbone <rtcarbone@gmail.com>
2025-05-18 23:33:07 -0300

    feat: add global exception handler and configure RabbitMQ message converter and template

--- notification/src/main/java/br/com/fiap/javacare/notification/config/RabbitMQConfig.java
+++ notification/src/main/java/br/com/fiap/javacare/notification/config/RabbitMQConfig.java
@@ -1,6 +1,10 @@
 package br.com.fiap.javacare.notification.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +16,20 @@ public class RabbitMQConfig {
     @Bean
     public Queue appointmentNotificationQueue() {
         return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory,
+                                                                               MessageConverter messageConverter) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter);
+        return factory;
     }
 
 }
